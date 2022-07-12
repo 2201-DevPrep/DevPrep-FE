255e7fb606bf8a98c37facbd5aa2206cafc8fe4b
Rio Foster <friotious@gmail.com>
2022-07-12 10:09:43 -0700

    Add Dropdown component file

---
+++ src/Components/deckDropdown/DeckDropdown.tsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+import './DeckDropdown.scss'
+
+const DeckDropdown = () => {
+    const dropdownRef = useRef(null)
+    const [isActive, setIsActive] = useState(false);
+    return (
+        <>
+        </>
+    );
+}
+
+export default DeckDropdown;
--- src/Components/nav/Nav.tsx
+++ src/Components/nav/Nav.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import "./Nav.scss"
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
 import { NavLink } from 'react-router-dom'
 
 
@@ -9,6 +11,11 @@ import { NavLink } from 'react-router-dom'
             <NavLink className='appName' to='/dashboard'>
                 <h2>DEVPREP</h2>
             </NavLink>
+            <DropdownButton id="dropdown-basic-button" title="Dropdown button">
+                <Dropdown.Item href="#/behavioral">Behavioral</Dropdown.Item>
+                <Dropdown.Item href="#/technical-fe">Technical FE</Dropdown.Item>
+                <Dropdown.Item href="#/technical-be">Technical BE</Dropdown.Item>
+            </DropdownButton>
             <NavLink className='flashcards-button nav-button' to='/flashcards'>
                 <button>Flashcards</button>
             </NavLink>
