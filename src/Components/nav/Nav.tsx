308d581572bee7cb8f9bce3111502344139436ce
Rio Foster <friotious@gmail.com>
2022-07-11 19:22:53 -0700

    Add Navlink to nav

--- src/Components/nav/Nav.tsx
+++ src/Components/nav/Nav.tsx
@@ -1,10 +1,24 @@
 import React from 'react';
+import "./Nav.scss"
+import { NavLink } from 'react-router-dom'
 
 
  const Nav = () => {
     return (
-        <div>
-            <h2>DEVPREP</h2>
+        <div className='nav'>
+            <NavLink className='appName' to='/dashboard'>
+                <h2>DEVPREP</h2>
+            </NavLink>
+            <NavLink className='flashcards-button nav-button' to='/flashcards'>
+                <button>Flashcards</button>
+            </NavLink>
+            <NavLink className='home-button nav-button' to='/dashboard'>
+                <button>Home</button>
+            </NavLink>
+            <NavLink className='signout-button nav-button' to='/login'>
+                <button>Signout</button>
+            </NavLink>
+
         </div>
 
     );
